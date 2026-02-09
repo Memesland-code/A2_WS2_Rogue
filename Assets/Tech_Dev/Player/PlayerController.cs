cd82f94bc42eb21faadaed7900d5f18440d2012c
Memesland-code <memes.landytb@gmail.com>
2026-02-09 11:39:36 +0100

    Fixed player jump and movements + stairs behaviours

--- Assets/Tech_Dev/Player/PlayerController.cs
+++ Assets/Tech_Dev/Player/PlayerController.cs
@@ -1,6 +1,5 @@
 using System;
 using Tech_Dev.Procedural;
-using UnityEditor.AssetImporters;
 using UnityEngine;
 
 namespace Tech_Dev.Player
@@ -10,7 +9,9 @@ namespace Tech_Dev.Player
 	    [SerializeField] private float _groundSpeed;
 	    [SerializeField] private float _airSpeed;
 
-	    [SerializeField] private float _jumpHeight;
+	    [Space(10)]
+	    [SerializeField] private float _jumpForce;
+	    [SerializeField] private float _gravityScale;
 	    [SerializeField] private float _jumpCooldown;
 	    [SerializeField] private GroundDetector _groundDetector;
 
@@ -22,7 +23,6 @@ namespace Tech_Dev.Player
 	    
 	    private Rigidbody _rb;
 	    private InputManager _inputs;
-	    private Vector3 _initialGravity;
 
 	    private float _jumpTimeDelta;
 
@@ -33,14 +33,11 @@ namespace Tech_Dev.Player
 		    _rb = GetComponent<Rigidbody>();
 		    _inputs = GetComponent<InputManager>();
 		    _jumpTimeDelta = 0;
-		    _initialGravity = Physics.gravity;
 	    }
 
 	    private void Update()
 	    {
-		    Vector3 verticalMove = Vector3.zero;
-		    
-		    // Check player facing direction and change on movement direction 
+			// Check player facing direction and change on movement direction 
 		    if (_inputs.Move.x < 0 && _isFacingRight)
 		    {
 			    _isFacingRight = false;
@@ -62,34 +59,27 @@ namespace Tech_Dev.Player
 			    _jumpTimeDelta = _jumpCooldown;
 		    }
 
+		    float jumpForce = Mathf.Sqrt(_jumpForce * -3 * (Physics.gravity.y * _gravityScale));
 		    // Detect if contact with ground ==> make jump
 		    if (_inputs.Jump && _groundDetector.Touched && _jumpTimeDelta <= 0.0f)
 		    {
-			    verticalMove = new Vector3(0, Mathf.Sqrt(_jumpHeight * -2.0f * _initialGravity.y), 0);
-			    //! _rb.linearVelocity = new Vector3(0, _jumpHeight, 0);
+			    _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 			    _jumpTimeDelta = _jumpCooldown;
 		    }
-
-		    /* For constant jump (Mario like)
-		    if (!_inputs.Jump && _rb.linearVelocity.y > 0)
-		    {
-			    _rb.linearVelocity = new Vector3(0, 0, 0);
-		    }
-		    */
 		    
 		    // Process values
 		    float realGravity = _rb.linearVelocity.y >= 0 ? _rb.linearVelocity.y : _rb.linearVelocity.y * _fastFallingFactor;
 		    realGravity = Mathf.Max(realGravity, -15);
 		    
 		    float realSpeed = _groundDetector.Touched ? _groundSpeed : _airSpeed;
-
+		    
 		    Vector3 horizontalMove = new(_inputs.Move.x * realSpeed, 0, 0);
 
 		    // Apply to physics
-		    _rb.linearVelocity = new Vector3(0, realGravity, 0) + horizontalMove + verticalMove;
+		    _rb.linearVelocity = new Vector3(0, realGravity, 0) + horizontalMove;
 
-
-
+		    
+		    // Interaction
 		    if (_inputs.Interact && _interactTimeDelta <= 0.0f)
 		    {
 			    var colliders = Physics.OverlapBox(transform.position, new Vector3(0.5f, 0.5f, 0.5f), Quaternion.Euler(0, 0, 0));
@@ -105,6 +95,13 @@ namespace Tech_Dev.Player
 		    }
 		    
 		    _interactTimeDelta -=  Time.deltaTime;
+	    }
+
+
+	    
+	    private void FixedUpdate()
+	    {
+		    _rb.AddForce(Physics.gravity * ((_gravityScale - 1) * _rb.mass));
 	    }
     }
 }
