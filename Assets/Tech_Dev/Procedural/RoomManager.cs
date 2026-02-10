28684e9fc3040d3af96742d569e3ae7ff5f0217a
Memesland-code <memes.landytb@gmail.com>
2026-02-10 16:20:26 +0100

    Started Room Manager to check spawners and spawn enemies on

--- Assets/Tech_Dev/Procedural/RoomManager.cs
+++ Assets/Tech_Dev/Procedural/RoomManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Tech_Dev.Procedural
@@ -8,5 +9,25 @@ namespace Tech_Dev.Procedural
 		
 		public Type Type;
 		public Difficulty _difficulty;
+		
+		public List<Transform> GetSpawners()
+		{
+			var spawnersList = new List<Transform>();
+			
+			foreach (Transform roomElement in transform)
+			{
+				if (roomElement.CompareTag("EnemiesSpawner"))
+				{
+					spawnersList.Add(roomElement.transform);
+				}
+			}
+			
+			return spawnersList;
+		}
+
+		public void InitRoom()
+		{
+			
+		}
 	}
 }
