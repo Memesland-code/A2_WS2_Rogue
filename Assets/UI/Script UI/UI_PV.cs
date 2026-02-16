8f29aa12892bf8a3fb8df459c3ce73e432af0625
FeronB <b.feron@game-sup.fr>
2026-02-16 20:45:48 +0100

    UI barre de vie
    correction script avec utilisation de fillAmount (Merci K

--- Assets/UI/Script UI/UI_PV.cs
+++ Assets/UI/Script UI/UI_PV.cs
@@ -1,35 +1,50 @@
+using Tech_Dev;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 using Slider = UnityEngine.UI.Slider;
 
 public class UI_PV : MonoBehaviour
 {
-//  [SerializeField]
-  [SerializeField] private RectTransform _barRect;
-  
-  [SerializeField] private RectMask2D _mask;
 
-  private float _maxRightMask;
-  private float _InitialRightMask;
-  
-  
-  
-  
-  void start()
+    [SerializeField] private float MaxHP;
+    [SerializeField] private float CurrentHP;
+    private float SaveCurrentHP;
+    
+    [SerializeField] private Image PvBar;
+    
+    [SerializeField] private TMP_Text PvCount;
+    
+  void Start()
   {
-     _maxRightMask = _barRect.rect.width - _mask.padding.x - _mask.padding.z;
-     _InitialRightMask = _mask.padding.z;
+      MaxHP = GameManager.GetPlayerScriptRef().GetMaxHealth();  //récupérer les valeurs dans les scripts resepctif
+      CurrentHP=GameManager.GetPlayerScriptRef().GetHealth(); // idem avec CurrentHP
+      
+      PvBar.fillAmount = CurrentHP / MaxHP;  // set le fillAmount de l'image  en des pv / pvMax 
+      PvCount.SetText($"{CurrentHP}/{MaxHP}"); // écrire le nom de pv / pvMax
+      SaveCurrentHP = CurrentHP; // save le nombre de pv pour en faire une comparaison plus tard voir pour le supprimer plus tard
      
   }
 
-  public void SetValue(int newValue)
+  void Update()
   {
- //     var targetWidth = newValue * _maxRightMask / _health.MaxHp;
-   //   var newRightMask = _maxRightMask + _InitialRightMask - targetWidth;
-     // var padding = _mask.padding;
-      //padding.z = newRightMask;
-      //_mask.padding = padding;
+      MaxHP = GameManager.GetPlayerScriptRef().GetMaxHealth();
+      CurrentHP=GameManager.GetPlayerScriptRef().GetHealth();
       
+      if (SaveCurrentHP != CurrentHP) // vérifier une modification de point de vie
+      {
+        PV_Change(); // lire ce qu'il y a d'écrit
+      }
+  }
+
+
+  void PV_Change()
+  {
+      SaveCurrentHP = CurrentHP; // remettre save PV = current PV
+      float PvPercent = CurrentHP / MaxHP; // set la valeur utiliser pour faire le pourcentage de point de vie
+      PvBar.fillAmount = PvPercent; // modifier l'iamge en fonction du pourcentage de point de vie
+      PvCount.SetText($"{CurrentHP}/{MaxHP}"); // afficher le nouveau nombre de point de vie
   }
 }
