43ae41dfd872542f03861b628c36c2ea59924ffe
FeronB <b.feron@game-sup.fr>
2026-02-16 14:36:53 +0100

    UI
    Ajout ui début barre de vie et script associé

---
+++ Assets/UI/Script UI/UI_PV.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+using Slider = UnityEngine.UI.Slider;
+
+public class UI_PV : MonoBehaviour
+{
+
+  [SerializeField]
+  private  float _health;  
+  [SerializeField] private RectTransform _barRect;
+  
+  [SerializeField] private RectMask2D _mask;
+
+  private float _maxRightMask;
+  private float _InitialRightMask;
+  
+  
+  
+  
+  void start()
+  {
+     _maxRightMask = _barRect.rect.width - _mask.padding.x - _mask.padding.z;
+     _InitialRightMask = _mask.padding.z;
+     
+  }
+
+  public void SetValue(int newValue)
+  {
+      var targetWidth = newValue * _maxRightMask / _health.MaxHp ; // remplacez MaxHp par l'équivalant dans le script de Kilyan
+      var newRightMask = _maxRightMask + _InitialRightMask - targetWidth;
+      var padding = _mask.padding;
+      padding.z = newRightMask;
+      _mask.padding = padding;
+      
+  }
+}
