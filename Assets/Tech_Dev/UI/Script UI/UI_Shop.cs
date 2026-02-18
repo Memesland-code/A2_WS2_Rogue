8fca422c4ccd1dcc67d0cb04591ca9a285a3271c
FeronB <b.feron@game-sup.fr>
2026-02-18 10:13:59 +0100

    Avancement UI
    shop + upgrade

--- Assets/Tech_Dev/UI/Script UI/UI_Shop.cs
+++ Assets/Tech_Dev/UI/Script UI/UI_Shop.cs
@@ -14,7 +14,6 @@ public class UI_Shop : MonoBehaviour
 
  [SerializeField] private int ActualShop;
  
- [SerializeField] private GameObject Background; // fond dérrière le shop et autres éléments intéractibles
  
  [SerializeField] private GameObject AllGreyChoice;
  [SerializeField] private GameObject GreyChoice1; // cadres qui recouvrirons les items une fois acheté
@@ -46,7 +45,6 @@ public class UI_Shop : MonoBehaviour
  [SerializeField] int Price6;
  
  
- public PlayerController playerController;
  private int CurrentGold;
 
  
@@ -58,7 +56,6 @@ public class UI_Shop : MonoBehaviour
  {
   Shop1.SetActive(false);
   Shop2.SetActive(false);
-  Background.SetActive(false); 
   
   GreyChoice1.SetActive(false); // met sur faux sur les cadres qui recouvrirons les items une fois acheté
   GreyChoice2.SetActive(false);
@@ -101,7 +98,6 @@ public class UI_Shop : MonoBehaviour
  public void OpenShop()
  {
   Time.timeScale = 0; // met le jeux et les animations en pause ATTNETION à bien retirer la pause ensuite
-  Background.SetActive(true);
 
   if (ActualShop == 1)
   {
@@ -124,7 +120,6 @@ public class UI_Shop : MonoBehaviour
   {
    Shop2.SetActive(false);
   }
-  Background.SetActive(false);
   
   
   Time.timeScale = 1; //   le jeu et les animation de la pause
