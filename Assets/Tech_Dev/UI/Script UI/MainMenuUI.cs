c250f6325f91bd90dbac1da507761ca30a9d3f8d
FeronB <b.feron@game-sup.fr>
2026-02-19 14:22:21 +0100

    UI main menu habillage

--- Assets/Tech_Dev/UI/Script UI/MainMenuUI.cs
+++ Assets/Tech_Dev/UI/Script UI/MainMenuUI.cs
@@ -5,11 +5,12 @@ namespace Tech_Dev.UI.Script_UI
     public class MainMenuUI : MonoBehaviour
     {
 	    [SerializeField] private GameObject _settingsScreen;
+
+
 	    
         public void StartGame()
         {
             gameObject.SetActive(false);
-
             GameManager.StartGame();
         }
 
